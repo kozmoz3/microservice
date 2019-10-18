fb18bc6f52769198922bc0096488d26dde77e6e5
kozmoz3 <kiqueyo847@gmail.com>
2019-10-18 17:41:04 -0500

    creando entity item

--- Udemy/springboot-servicio-items/src/main/java/com/item/service/model/Item.java
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/model/Item.java
@@ -4,8 +4,35 @@ import java.util.Date;
 
 public class Item {
 
-	private Long id;
-	private String producto;
-	private Double precio;
-	private Date createAt;
+	private Producto producto;
+	private Integer cantidad;
+
+	public Item() {
+	}
+
+	public Item(Producto producto, Integer cantidad) {
+		super();
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Double getTotal() {
+		return producto.getPrecio() * cantidad.doubleValue();
+	}
 }
--- Udemy/springboot-servicio-items/src/main/java/com/item/service/model/Producto.java
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/model/Producto.java
@@ -1,5 +1,38 @@
 package com.item.service.model;
 
+import java.util.Date;
+
 public class Producto {
 
+	private Long id;
+	private String producto;
+	private Double precio;
+	private Date createAt;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getProducto() {
+		return producto;
+	}
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
 }
