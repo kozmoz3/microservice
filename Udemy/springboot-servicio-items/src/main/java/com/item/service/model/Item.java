7bc40e6a3adde27be44b25495040a26c4c1d97dc
kozmoz3 <kiqueyo847@gmail.com>
2019-10-18 17:35:06 -0500

    creando producto

---
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/model/Item.java
@@ -0,0 +1,11 @@
+package com.item.service.model;
+
+import java.util.Date;
+
+public class Item {
+
+	private Long id;
+	private String producto;
+	private Double precio;
+	private Date createAt;
+}
---
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/model/Producto.java
@@ -0,0 +1,5 @@
+package com.item.service.model;
+
+public class Producto {
+
+}
