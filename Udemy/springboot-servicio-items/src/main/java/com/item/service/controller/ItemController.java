245f6a677b0fb399b8160e4e08a1200049e6d4d0
kozmoz3 <kiqueyo847@gmail.com>
2019-10-18 23:34:47 -0500

    creando controlador

---
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/controller/ItemController.java
@@ -0,0 +1,28 @@
+package com.item.service.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.item.service.model.Item;
+import com.item.service.service.ItemService;
+
+@RestController
+public class ItemController {
+
+	@Autowired
+	ItemService itemService;
+	
+	@GetMapping("/listar")
+	public List<Item>listarItem(){
+		return itemService.findAll();
+	}
+	
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+}
