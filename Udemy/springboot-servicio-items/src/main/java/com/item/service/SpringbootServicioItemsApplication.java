ae91ef4682005fdfd78d14e497f4c217a3ed119b
kozmoz3 <kiqueyo847@gmail.com>
2019-10-19 16:54:28 -0500

    quitar ribbon de item

--- Udemy/springboot-servicio-items/src/main/java/com/item/service/SpringbootServicioItemsApplication.java
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/SpringbootServicioItemsApplication.java
@@ -3,11 +3,11 @@ package com.item.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
-@RibbonClient(name="servicio-productos")
+//@RibbonClient(name="servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemsApplication {
