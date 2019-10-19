7335eec00a5581ca6d00588274b8e72262456389
kozmoz3 <kiqueyo847@gmail.com>
2019-10-19 13:50:44 -0500

    usando cliente feing para consumir api producto

--- Udemy/springboot-servicio-items/src/main/java/com/item/service/cliente/ProductoClienteRest.java
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/cliente/ProductoClienteRest.java
@@ -1,5 +1,23 @@
 package com.item.service.cliente;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.item.service.model.Producto;
+
+
+
+@FeignClient(name="servicio-productos",url="localhost:8001")
 public interface ProductoClienteRest {
+	
+	@GetMapping("/listar")
+	public List<Producto> listar();
+	
+	@GetMapping("/ver/{id}")
+	public Optional<Producto> detalle(@PathVariable Long id);
 
 }
---
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/service/impl/ItemServiceFeingImpl.java
@@ -0,0 +1,30 @@
+package com.item.service.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.item.service.cliente.ProductoClienteRest;
+import com.item.service.model.Item;
+import com.item.service.model.Producto;
+import com.item.service.service.ItemService;
+
+@Service
+public class ItemServiceFeingImpl  implements ItemService{
+
+	@Autowired
+	ProductoClienteRest cliente;
+	
+	@Override
+	public List<Item> findAll() {
+		return cliente.listar().stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Item findById(Long id, Integer cantidad) {
+		return null;// new Item(cliente.detalle(id), cantidad);
+	}
+
+}
