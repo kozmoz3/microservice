c16fc32b65dbe6398a6dcbff14cd099f96ca95b7
kozmoz3 <kiqueyo847@gmail.com>
2019-10-18 23:26:13 -0500

    implementando el restemplate en los servicios del item

--- Udemy/springboot-servicio-items/src/main/java/com/item/service/service/impl/ItemServiceImpl.java
+++ Udemy/springboot-servicio-items/src/main/java/com/item/service/service/impl/ItemServiceImpl.java
@@ -1,30 +1,40 @@
 package com.item.service.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.item.service.model.Item;
+import com.item.service.model.Producto;
 import com.item.service.service.ItemService;
 
 @Service
-public class ItemServiceImpl implements ItemService{
+public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private RestTemplate clienteRest;
-	
+
 	@Override
 	public List<Item> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Producto> listProducto = Arrays
+				.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class));
+		return listProducto.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
 	@Override
 	public Item findById(Long id, Integer cantidad) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> pathVariable = new HashMap<String, String>();
+		pathVariable.put("id", id.toString());
+		Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariable);
+		
+		return new Item(producto, cantidad);
 	}
 
 }
