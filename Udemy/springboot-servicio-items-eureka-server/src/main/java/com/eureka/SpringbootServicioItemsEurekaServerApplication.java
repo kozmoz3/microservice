50099d1f4d5355543e352934144447c37d8b73ca
kozmoz3 <kiqueyo847@gmail.com>
2019-10-19 16:14:04 -0500

    Creando servidor eureka

---
+++ Udemy/springboot-servicio-items-eureka-server/src/main/java/com/eureka/SpringbootServicioItemsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioItemsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioItemsEurekaServerApplication.class, args);
+	}
+
+}
